a980f28a38925537784bdf113a1f0a70e73f5fc7
Matthew Batchelder <borkweb@gmail.com>
2013-12-12 12:12:05 -0500

    Updating notes

--- components/templates/admin.php
+++ components/templates/admin.php
@@ -2,11 +2,11 @@
 	<h2>Copy Active Plugins</h2>
 	<div class="content-container">
 		<h3>Current Active Plugins (Read Only)</h3>
-		<p>Copy this block to save the current layout for archiving or applying to another blog.</p>
+		<p>Copy this block to save the current active plugin list for archiving or applying to another site.</p>
 		<textarea cols="100" rows="15"><?php echo esc_textarea( $current ); ?></textarea><br/>
 
 		<h3>Replace Active Plugin List</h3>
-		<p>Paste a saved layout here to override the current layout.</p>
+		<p>Paste a saved active plugin list here to override the current active plugins (yes, plugins will be deactivated if they aren't in the list).</p>
 		<form action="plugins.php?page=copy-active-plugins" method="post">
 			<?php wp_nonce_field( 'go-copy-active-plugins-override', '_go_copy_active_plugins_override_nonce' ); ?>
 			<textarea cols="100" rows="15" name="plugins"></textarea><br/>
